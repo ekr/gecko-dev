60e63bf70c929ecb1af08301b8b08a6ddf78875d
EKR <ekr@rtfm.com>
2014-03-06 21:49:21 +0000

    Cleanup some log files. Add instrumentation for shmem

--- content/media/gmp/GMPVideoDecoderChild.cpp
+++ content/media/gmp/GMPVideoDecoderChild.cpp
@@ -104,8 +104,6 @@ GMPVideoDecoderChild::RecvDecode(const GMPVideoEncodedFrameImpl& inputFrame,
   GMPVideoEncodedFrameImpl* tmp =
     (GMPVideoEncodedFrameImpl*)&inputFrame;
 
-  fprintf(stderr, "%s: in length = %d\n", __FUNCTION__, tmp->Size());
-
   // We need a mutable copy of the decoded frame, into which we can inject
   // the shared memory backing.
   auto frame = new GMPVideoEncodedFrameImpl();
